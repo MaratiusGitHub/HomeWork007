5bf78637ec006d01a42ad73a258635541f2ba0ba
Maratius <maratius.ru@gmail.com>
2022-09-06 01:34:57 +0300

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+
+
+int[,] NewRandomArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetResultArray(int[,] numbers)
+{
+    double[] result = new double[numbers.GetLength(1)];
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            sum += numbers[i, j];
+        }
+        result[j] = Math.Round(sum / numbers.GetLength(0), 2);
+    }
+    return result;
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = NewRandomArray(rows, columns, 0, 50);
+
+PrintArray(myArray);
+
+double[] result = GetResultArray(myArray);
+
+Console.WriteLine($"Среднее арифмитеческое каждого столбца = {String.Join("; ", result)}");
